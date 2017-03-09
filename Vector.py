86eb5785ea7a3a417a3d711498b380ef0c39e7ec
David Buck <david.x.buck@icloud.com>
2017-03-09 17:02:31 +0800

    Add files via upload

--- Vector.py
+++ Vector.py
@@ -40,14 +40,26 @@ class Vector(object):
             direction = self.scalar_multiple(1./self.magnitude())
             return (direction)
         
-    except ZeroDivisionError:
-        raise Exception('cannot normalize the zero vector')
+        except ZeroDivisionError:
+            raise Exception('cannot normalize the zero vector')
+        
+    def dot_product(self,v):
+        dotp = sum([x*y for x,y in zip(self.coordinates, v.coordinates)])
+        return (dotp)
+    
+    def angle(self,v):
+        xmag = self.magnitude()
+        ymag = v.magnitude()
+        angle = math.acos(self.dot_product(v)/(xmag*ymag))
+        return (angle, math.degrees(angle))
             
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
     
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+    
+import math
     
 vec1a = Vector([8.218, -9.341])
 vec1b = Vector([-1.129, 2.111])
@@ -69,3 +81,15 @@ print(vec4a.magnitude())
 print(vec4b.magnitude())
 print(vec4c.normalized())
 print(vec4d.normalized())
+vec5a = Vector([7.887, 4.138])
+vec5b = Vector([-8.802, 6.776])
+vec6a = Vector([-5.955, -4.904, -1.874])
+vec6b = Vector([-4.496, -8.755, 7.103])
+vec7a = Vector([3.183, -7.627])
+vec7b = Vector([-2.668, 5.319])
+vec8a = Vector([7.35, 0.221, 5.188])
+vec8b = Vector([2.751, 8.259, 3.985])
+print(vec5a.dot_product(vec5b))
+print(vec6a.dot_product(vec6b))
+print(vec7a.angle(vec7b))
+print(vec8a.angle(vec8b))
